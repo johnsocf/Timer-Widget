b7cb42915c265f2961be427e95e04c66e3a8e8ba
Catherine Johnson <cfjohnson@earthlink.net>
2013-10-24 11:43:58 -0600

    modularized a function for buffering the slider and implemented that for event handlers using it

--- js/timer.js
+++ js/timer.js
@@ -8,6 +8,9 @@
 		$downButton = $('#downButton'),
 		$powerButton = $('#powerButton'),
 		$timeUnits = $('#timeUnits'),
+		$switchButton = $('#switch'),
+		$powerOn = $('#on-button'),
+		$powerOff = $('#power-off-display'),
 		scale = scaleSlider.value,
 		scale = 0,
 		MINIMUM_SCALE = 0,
@@ -16,6 +19,14 @@
 		minutes = 60,
 		currentTime,
 		refreshInterval,
+		font_height = 15,
+		margin = 35,
+		loop,
+		hand_truncation = canvas.width/25,
+		hour_hand_truncation = canvas.width/10,
+		numeral_spacing = 20,
+		radius = canvas.width/2 - margin,
+		hand_radius = radius + numeral_spacing,
 		hour = (text/minutes).toFixed(2);
 
 
@@ -25,48 +36,33 @@
 		powerOn();
 	}
 	
-	$(scaleSlider).change(function(e){
+	$scaleSlider.change(function(e){
 		text = e.target.value;
 	    slideAdjust(text);
-	    var val = $(this).val();
-	    var buf = ((100 - val)/ 15) + parseInt(val);
-	    $(this).css(
-	      'background',
-	      'linear-gradient(to right, #9AD3DF 0%, #9AD3DF ' + val/30 + '%, #000 ' + val/29 + '%, #000 ' + buf + '%, #000 ' + buf + '%, #000 100%)'
-	    );
+	    bufferSlider(text);
 	});
 
-	$scaleSlider.change(function() {
-	    var val = $(this).val();
-	    var buf = ((100 - val)/ 15) + parseInt(val);
-	    $(this).css(
-	      'background',
-	      'linear-gradient(to right, #9AD3DF 0%, #9AD3DF ' + val/30 + '%, #000 ' + val/29 + '%, #000 ' + buf + '%, #000 ' + buf + '%, #000 100%)'
-	    );
-  	});
-
-
-	$(upButton).mousedown(function(e){
+	$upButton.mousedown(function(e){
 		refreshInterval = setInterval(increaseCount,30)
 	}).mouseup(function(e){
 		clearInterval(refreshInterval);
 		refreshInterval = 0;
 	});
 
-	$(downButton).mousedown(function(e){
+	$downButton.mousedown(function(e){
 		refreshInterval = setInterval(decreaseCount,30)
 	}).mouseup(function(e){
 		clearInterval(refreshInterval);
 		refreshInterval = 0;
 	});
 
-	$(powerButton).click(function (){
-		$('#scaleOutput').toggle(function(){});
-		$('#power-off-display').toggle();
-		$('#timeUnits').toggle();
-		$('#on-button').toggleClass("blue-button");
-		$('#on-button').toggleClass("white-button");
-		$('#scaleSlider').css(
+	$powerButton.click(function (){
+		$scaleOutput.toggle(function(){});
+		$powerOff.toggle();
+		$timeUnits.toggle();
+		$powerOn.toggleClass("blue-button");
+		$powerOn.toggleClass("white-button");
+		$scaleSlider.css(
 	      'background',
       'linear-gradient(to right, #000 0%, #000 ' + 0/28 + '%, #000 ' + 0/28 + '%, #000 ' + 0 + '%, #000 ' + 0 + '%, #000 100%)'
     );
@@ -76,39 +72,39 @@
 		moveSlider(currentTime);
 	});
 
-	$('#switch').change(function(){
+	$switchButton.change(function(){
 		self = $(this);
 		time = text,
 		minutes = 60,
 		hour = (text/minutes).toFixed(2);
 
 
-		if($('#switch').prop('checked')) {
+		if($switchButton.prop('checked')) {
 		    time = hour;
 	    	$scaleOutput.text(time);
 	    	$timeUnits.text("hrs");
 	    	if (time>20) {
-	    		$(scaleOutput).css("left","29%");
+	    		$scaleOutput.css("left","29%");
 	    	}
 	    	else {
-	    		$(scaleOutput).css("left","35%");
+	    		$scaleOutput.css("left","35%");
 	    	}
 		} else {
     		$scaleOutput.text(time);
     		$timeUnits.text("min");
     		if (time < 2000) {
-	    		$(scaleOutput).css("left","38%");
+	    		$scaleOutput.css("left","38%");
 	    	}
 	    	else {
-	    		$(scaleOutput).css("left","33%");
+	    		$scaleOutput.css("left","33%");
 	    	}
 		}
 	});
 
 	function slideAdjust(text) {
 		if (text < MINIMUM_SCALE) text = MINIMUM_SCALE;
-	   else if (text > MAXIMUM_SCALE) text = MAXIMUM_SCALE;
-	   drawText(text);
+	    else if (text > MAXIMUM_SCALE) text = MAXIMUM_SCALE;
+	    drawText(text);
 	}
 
 	function powerOn () {
@@ -122,18 +118,16 @@
 		var time = text,
 			hour = (text/minutes).toFixed(2);
 		
-	    if($('#switch').prop('checked')) {
+	    if($switchButton.prop('checked')) {
 	    	time = hour;
 	    	$scaleOutput.text(time);
 	    	$timeUnits.text("hrs");
 	    	currentTime = text;
 	    	if (time>20) {
 	    		$(scaleOutput).css("left","29%");
-	    		console.log(time);
 	    	}
 	    	else {
 	    		$(scaleOutput).css("left","35%");
-	    		console.log(time);
 	    	}
 	    }
 	    else {
@@ -141,14 +135,13 @@
 	    	$timeUnits.text("min");
 	    	currentTime = text;
 	    	if (time < 2000) {
-	    		$(scaleOutput).css("left","38%");
+	    		$scaleOutput.css("left","38%");
 	    	}
 	    	else {
-	    		$(scaleOutput).css("left","33%");
+	    		$scaleOutput.css("left","33%");
 	    	}
 	    }
 	}	
-
 
 	function increaseCount () {
 		text ++;
@@ -167,42 +160,49 @@
 	}
 
 	function moveSlider(text) {
-		$('#scaleSlider').val(currentTime);
+		$scaleSlider.val(currentTime);
+	    bufferSlider(text);
 	}
-	
 
-	
+	function bufferSlider(text) {
+		var val = $scaleSlider.val(),
+	    	buf = ((100 - val)/ 15) + parseInt(val);
+		if(text < 200) {
+	    	$scaleSlider.css(
+			      'background',
+			      'linear-gradient(to right, #9AD3DF 0%, #9AD3DF ' + val/15 + '%, #000 ' + val/15 + '%, #000 ' + buf + 10 + '%, #000 ' + buf + '%, #000 100%)'
+			    ); 
+	    	console.log("here");}
+	    else if(200 < text && text < 470) {
+	    	$scaleSlider.css(
+			      'background',
+			      'linear-gradient(to right, #9AD3DF 0%, #9AD3DF ' + val/20 + '%, #000 ' + val/20 + '%, #000 ' + buf +10 + '%, #000 ' + buf + '%, #000 100%)'
+			    ); 
+	    	console.log("here2");
+	    	console.log(text);
+	    }	
+		else {$scaleSlider.css(
+			      'background',
+			      'linear-gradient(to right, #9AD3DF 0%, #9AD3DF ' + val/29 + '%, #000 ' + val/29 + '%, #000 ' + buf + '%, #000 ' + buf + '%, #000 100%)'
+			    );
+			console.log("here3");}
+	}
 
+	function drawCircle() {
+		context.beginPath();
+		context.arc(canvas.width/2, canvas.height/2, radius, 0, Math.PI*2, true);
+		context.lineWidth = "5";
+		context.stroke();
+	}	
 
+	function drawCenter() {
+		context.beginPath();
+		context.arc(canvas.Width/2, canvas.height/2, 5, 0, Math.PI*2, true);
+		context.fill();
+	}
 
-	var font_height = 15,
-	margin = 35,
-	loop,
-	hand_truncation = canvas.width/25,
-	hour_hand_truncation = canvas.width/10,
-	numeral_spacing = 20,
-	radius = canvas.width/2 - margin,
-	hand_radius = radius + numeral_spacing;
-
-function drawCircle() {
-	context.beginPath();
-	context.arc(canvas.width/2, canvas.height/2, radius, 0, Math.PI*2, true);
-	context.lineWidth = "5";
-	context.stroke();
-}	
-
-
-
-
-
-function drawCenter() {
-	context.beginPath();
-	context.arc(canvas.Width/2, canvas.height/2, 5, 0, Math.PI*2, true);
-	context.fill();
-}
-
-function drawHand(loc, isHour) {
-	var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
+	function drawHand(loc, isHour) {
+		var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
 		handRadius = isHour ? radius - hand_truncation - hour_hand_truncation : radius - hand_truncation;
 		context.moveTo(canvas.width/2, canvas.height/2);
 		context.lineTo(canvas.width/2 + Math.cos(angle)*handRadius,
@@ -210,28 +210,28 @@ function drawHand(loc, isHour) {
 		context.strokeStyle = "#fff";
 		context.lineWidth = "2";
 		context.stroke();
-}
+	}
 
-function drawHands() {
-	var numberSeconds = text,
-	numberHours = text / 60,
-	extraMinutes = text % 60;
-	hour = hour > 12 ? hour - 12 : hour;
-	drawHand(numberHours, true, 0.5);
-	drawHand(extraMinutes, false, 0.5);
-	drawHand(numberSeconds, false, 0.2);
-	drawHand((numberSeconds/60), false, 0.2);
-}
+	function drawHands() {
+		var numberSeconds = text,
+		numberHours = text / 60,
+		extraMinutes = text % 60;
+		hour = hour > 12 ? hour - 12 : hour;
+		drawHand(numberHours, true, 0.5);
+		drawHand(extraMinutes, false, 0.5);
+		drawHand(numberSeconds, false, 0.2);
+		drawHand((numberSeconds/60), false, 0.2);
+	}
 
-function drawClock() {
-	context.clearRect(0,0,canvas.width, canvas.height);
-	drawCircle();
-	drawCenter();
-	drawHands();
-}
+	function drawClock() {
+		context.clearRect(0,0,canvas.width, canvas.height);
+		drawCircle();
+		drawCenter();
+		drawHands();
+	}
 
-context.font = font_height + 'px Arial';
-loop = setInterval(drawClock, 1000);
+	context.font = font_height + 'px Arial';
+	loop = setInterval(drawClock, 1000);
 
 
 
